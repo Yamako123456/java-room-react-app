1121d826d967c9c3dc14f71d8c64f4fca81e2ac8
miyoko yamakawa <yamakodev@gmail.com>
2024-06-22 02:48:50 -0700

    Adding new video in real time ok

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -5,19 +5,24 @@ function AdminPage({ tutorials, setTutorials }) {
     let maxId = tutorials.length;
 
     const addVideo = () => {
-        console.log('I am in the addVideo function');
         const newVideo = {
             id: maxId + 1,
             title: "Java Tutorial for Beginners 2023" + (maxId + 1),
             name: "New Video" + (maxId + 1),
-            url: "https://www.google.com/search?q=java%20tutorial%20site%3Ayoutube.com&sca_esv=4c1f4100c44949b9&rlz=1C1RXQR_enUS959US959&sxsrf=ADLYWIKKztL-nQSZZEh4GlwzEWlFUQOUmQ%3A1719024934950&ei=LT12ZumXOZPeur8PleaRoAQ&ved=2ahUKEwip4pj4mu6GAxUTr-4BHRVzBEQQ2wF6BAgJEAE&uact=5&oq=outube.com&gs_lp=Egxnd3Mtd2l6LXNlcnAiCm91dHViZS5jb20yDRAAGIAEGLEDGIMBGAoyDRAAGIAEGLEDGIMBGAoyChAAGIAEGLEDGAoyChAAGIAEGLEDGAoyDRAAGIAEGLEDGIMBGAoyDRAAGIAEGLEDGIMBGAoyDRAAGIAEGLEDGIMBGAoyBxAAGIAEGAoyBxAAGIAEGAoyChAAGIAEGLEDGApIhhBQAFiQDnAAeAGQAQCYAVagAYoFqgECMTC4AQPIAQD4AQGYAgqgAqgFwgIPECMYgAQYJxiKBRhGGPkBwgIKECMYgAQYJxiKBcICBBAjGCfCAgoQABiABBhDGIoFwgILEAAYgAQYsQMYgwHCAhEQLhiABBixAxjRAxiDARjHAcICBRAAGIAEwgInEAAYgAQYigUYRhj5ARiXBRiMBRjdBBhGGPkBGPQDGPUDGPYD2AEBwgIQEC4YgAQY0QMYQxjHARiKBcICEBAAGIAEGLEDGEMYgwEYigXCAgcQIxixAhgnwgIWEC4YgAQYsQMY0QMYQxiDARjHARiKBcICBhAAGAMYCpgDALoGBggBEAEYE5IHAjEwoAfYWw&sclient=gws-wiz-serp#",
-            thumbnail: "https://yt3.googleusercontent.com/o4MZVi2qdNgx0K7vpEl7DT2PefjROdisZTD7is6TMimF1_DTM49O1ld3iQzTpE5MCj86c-vzav8=s176-c-k-c0x00ffffff-no-rj",
+            url: selectedVideo,
+            thumbnail: process.env.PUBLIC_URL + "/img/sample3.jpg",
             comment: "new video on Java",
         }
         //props.tutorials.push(newVideo);
 
         setTutorials(tutorials => [...tutorials, newVideo])
     }
+    let selectedVideo = '';
+
+    const videoSelected = (event) => {
+        selectedVideo = event.target.value;
+    }
+
     return (
         <div class="container">
 
@@ -27,12 +32,23 @@ function AdminPage({ tutorials, setTutorials }) {
                         Your Videos
                     </div>
                     <div className='card-body'>
-                        hello world! This is admin page.
 
                         <VideoTable tutorials={tutorials} />
+
                         <button className="btn btn-primary" onClick={addVideo}>
                             Add New Video
                         </button>
+
+                        <label for="videos">Choose a video to add:</label>
+                        <select id="videos" name="videos" onChange={videoSelected} >
+                            <option value="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4">Big Buck Bunn</option>
+                            <option value="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4">For Bigger Blazes</option>
+                            <option value="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4">"Elephant Dream</option>
+                            <option value="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4">"For Bigger Joyrides"</option>
+                            <option value="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4">"Sintel"</option>
+                            <option value="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4">"Tears of Steel"</option>
+
+                        </select>
 
                     </div>
                 </div>
